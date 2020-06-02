2cc66d786008ab71f4808167d7de50775aa05361
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-06-03 01:10:45 +0300

    feat(icon-registry): add icon-registry module

---
+++ src/app/core/icon-registry/icon-registry.module.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { IconRegistryConfig } from './icon-registry.interface';
+import { ICON_REGISTRY_CONFIG } from './icon-registry.token';
+import { IconRegistryService } from './icon-registry.service';
+
+@NgModule({})
+export class IconRegistryModule {
+  static forRoot(config: IconRegistryConfig): ModuleWithProviders {
+    return {
+      ngModule: IconRegistryModule,
+      providers: [
+        IconRegistryService,
+        {
+          provide: ICON_REGISTRY_CONFIG,
+          useValue: config,
+        },
+      ],
+    };
+  }
+}
