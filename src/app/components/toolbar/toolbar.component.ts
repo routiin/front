4351dbfbee7cdd5d8496ed5142299b74cbbc2f2b
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-06-04 20:15:35 +0300

    chore: add material icons

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToolbarComponent implements OnInit {
-  constructor() {}
+export class ToolbarComponent {
+  icons: { [name: string]: string } = {
+    today: 'track_changes',
+    feed: 'visibility',
+    add: 'control_point',
+    progress: 'bar_chart',
+    profile: 'face',
+  };
 
-  ngOnInit(): void {}
+  get buttons() {
+    return Object.keys(this.icons);
+  }
 }
