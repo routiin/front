7fb619cae2c9037432dd853cd22d868d78ee4ae2
Andrey Podkovyrov <a.podkovyrov@yandex.ru>
2020-06-04 19:34:08 +0300

    chore: upd icons, header, footer

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
